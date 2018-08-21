c23f2de25dcf3bff975243e0d81ee28b09091806
Alfredo Re <alfredoreduarte@gmail.com>
2018-08-21 10:26:30 -0400

    Added basic styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :initialize_entities
 
   def index
+  	
+  end
+
+  def initialize_entities
   	@organizations = current_user.organizations
   	@projects = current_user.projects
   	@ideas = current_user.ideas
