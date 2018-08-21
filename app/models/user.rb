f2b8a287dc928fa1d175755b97d68f0e62de463f
Alfredo Re <alfredoreduarte@gmail.com>
2018-08-21 18:40:55 -0400

    Added email confirmation and dev mailing settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 	devise :database_authenticatable, :registerable,
-		 :recoverable, :rememberable, :validatable
+		 :recoverable, :rememberable, :validatable, :confirmable, :trackable
 	has_many :memberships
 	has_many :organizations, through: :memberships
 	has_many :projects
